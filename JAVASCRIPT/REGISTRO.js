01f039cb43946f2fb1adaf32a6b088daf0b97b9c
Victor Zambrano <victorzanmar.25@gmail.com>
2025-05-27 12:08:11 -0500

    RegistroClientes

---
+++ JAVASCRIPT/REGISTRO.js
@@ -0,0 +1,43 @@
+function validarFormulario() {
+    var cedula = document.getElementById("cedula").value.trim();
+    var apellidos = document.getElementById("apellidos").value.trim();
+    var nombres = document.getElementById("nombres").value.trim();
+    var telefono = document.getElementById("telefono").value.trim();
+    var email = document.getElementById("email").value.trim();
+
+    if (
+        cedula === "" ||
+        apellidos === "" ||
+        nombres === "" ||
+        telefono === "" ||
+        email === ""
+    ){
+        alert("POR FAVOR, COMPLETE TODOS LOS CAMPOS");
+        return false;
+    }
+
+    // Crea objeto cliente
+    var cliente = {
+        cedula: cedula,
+        apellidos: apellidos,
+        nombres: nombres,
+        telefono: telefono,
+        email: email
+    };
+
+    // Obtener clientes existentes 
+    var clientes = JSON.parse(localStorage.getItem("clientes")) || [];
+
+    // Agrega nuevo cliente
+    clientes.push(cliente);
+
+    // Guarda en localStorage
+    localStorage.setItem("clientes", JSON.stringify(clientes));
+
+    alert("CLIENTE REGISTRADO CORRECTAMENTE");
+
+    // Limpia formulario
+    document.getElementById("formRegistro").reset();
+
+    return false;
+}
